f85de38819f8fe38502aa29dafb6de5377243fee
wwcollins <williamwcollinsjr@gmail.com>
2023-06-03 13:59:17 -0500

    Commit of app rebuild

--- cover_letter_generator.py
+++ cover_letter_generator.py
@@ -329,7 +329,7 @@ def main():
     col1, col2 = st.columns(2)
     with col1:
         st.sidebar.image(f'./images/you_image.jpg', 'William Collins', width=150)
-        download_resume(loc='sidebar')
+        download_resume("", loc='sidebar')
     with col2:
         st.sidebar.caption(f'Location: Austin, TX' + '\n' +
                            f'\n [Email]("mailto:williamwcollinsjr@gmail.com)'
